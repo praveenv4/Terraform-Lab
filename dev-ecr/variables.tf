902c692bdfe3276ebc5e6dfbb41d2ecbf48258e5
praveenv4 <98619977+praveenv4@users.noreply.github.com>
2022-09-18 19:11:20 -0400

    Create variables.tf

---
+++ dev-ecr/variables.tf
@@ -0,0 +1,21 @@
+variable "ecr_name" {
+  description = "The list of ecr names to create"
+  type        = list(string)
+  default     = null
+}
+variable "tags" {
+  description = "The key-value maps for tagging"
+  type        = map(string)
+  default     = {}
+}
+variable "image_mutability" {
+  description = "Provide image mutability"
+  type        = string
+  default     = "MUTABLE"
+}
+
+variable "encrypt_type" {
+  description = "Provide type of encryption here"
+  type        = string
+  default     = "KMS"
+}
