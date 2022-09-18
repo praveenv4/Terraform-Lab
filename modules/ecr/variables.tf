ff9724ba83f9263b479011da960db94c224f44f9
praveenv4 <98619977+praveenv4@users.noreply.github.com>
2022-09-18 19:03:32 -0400

    Create variables.tf

---
+++ modules/ecr/variables.tf
@@ -0,0 +1,12 @@
+resource "aws_ecr_repository" "nii" {
+  for_each             = toset(var.ecr_name)
+  name                 = each.key
+  image_tag_mutability = var.image_mutability
+  encryption_configuration {
+    encryption_type = var.encrypt_type
+  }
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+  tags = var.tags
+}
