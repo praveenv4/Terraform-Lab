84d5430cbf1478019af64eb61fe978ab5e6e7467
praveenv4 <98619977+praveenv4@users.noreply.github.com>
2022-09-18 20:29:03 -0400

    Create backend.tf

---
+++ dev-ecr/backend.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "aws" {
+  region  = "enter-your-aws-region"
+  profile = "enter-your-aws-profile"
+}
