267a23f9b412813e540fa15fe26e00949a6236d1
praveenv4 <98619977+praveenv4@users.noreply.github.com>
2022-09-18 21:14:51 -0400

    Update main.tf

--- modules/ecr/main.tf
+++ modules/ecr/main.tf
@@ -1,4 +1,4 @@
-resource "aws_ecr_repository" "nii" {
+resource "aws_ecr_repository" "ecr" {
   for_each             = toset(var.ecr_name)
   name                 = each.key
   image_tag_mutability = var.image_mutability
