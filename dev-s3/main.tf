f4f83800f0d2119f126ac45b550f4c568eefd1fb
praveenv4 <98619977+praveenv4@users.noreply.github.com>
2022-09-18 23:31:24 -0400

    Update main.tf

--- dev-s3/main.tf
+++ dev-s3/main.tf
@@ -1,6 +1,6 @@
 module "dev-s3" {
   source     = "../modules/s3"
-  s3_buckets = var.dev_s3_bucket
+  s3_buckets = var.s3_buckets
   tags       = var.tags
   versioning = var.versioning
   acl        = var.s3_acl
