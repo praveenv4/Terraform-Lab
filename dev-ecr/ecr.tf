03bdae078216e5dd9cf6cf5a0c4defaf30a2ce8d
praveenv4 <98619977+praveenv4@users.noreply.github.com>
2022-09-18 23:17:48 -0400

    Update ecr.tf

--- dev-ecr/ecr.tf
+++ dev-ecr/ecr.tf
@@ -1,5 +1,5 @@
 module "ecr-repo" {
-  source           = "./../../modules/ecr"
+  source           = "./../modules/ecr"
   ecr_name         = var.ecr_name
   tags             = var.tags
   image_mutability = var.image_mutability
